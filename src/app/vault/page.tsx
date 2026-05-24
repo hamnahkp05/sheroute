1258a7ba97541eca7479ed39a2127f0496558f05
hamnahkp05 <hamnahkp05@gmail.com>
2026-05-24 21:57:45 +0000

    Reapply "vault fixed"
    This reverts commit 3c9597481adf1ef79bd530f60a6be3f5a129563b.

---
+++ src/app/vault/page.tsx
@@ -0,0 +1,8 @@
+export default function VaultPage() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Vault Page</h1>
+      <p>Vault is loading successfully 🚀</p>
+    </div>
+  );
+}
